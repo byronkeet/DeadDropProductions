337fbd4185af8d1493f5deb0bcd0a85c690f50b6
byronius-maximus <42702782+byronius-maximus@users.noreply.github.com>
2019-03-26 20:12:47 +0700

    add env variable for key

--- config/s3.env.js
+++ config/s3.env.js
@@ -1,7 +1,7 @@
 var env = {
-    AWS_ACCESS_KEY: '',
-    AWS_SECRET_ACCESS_KEY: '',
-    REGION: '',
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    REGION: 'us-east-2',
     Bucket: 'keetbisbucket1'
 };
 
