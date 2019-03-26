ceb0ebae6b01fdba5bebf725199878560be0f885
byronius-maximus <42702782+byronius-maximus@users.noreply.github.com>
2019-03-26 21:09:30 +0700

    install and require dotenv. Make env file

--- config/s3.env.js
+++ config/s3.env.js
@@ -1,6 +1,9 @@
+var dotenv = require("dotenv");
+dotenv.config();
+
 var env = {
-    AWS_ACCESS_KEY: 'AKIAILNY774FECHXUBSA',
-    AWS_SECRET_ACCESS_KEY: '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',
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     REGION: 'us-east-2',
     Bucket: 'keetbisbucket1'
 };
